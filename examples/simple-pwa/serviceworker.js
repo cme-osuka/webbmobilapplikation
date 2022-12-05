c7c32f59527cd3cc56c6c35419a0dfb77bbe4bbe
Oscar Nilsson <oscar@osuka.dev>
2022-12-05 10:08:51 +0100

    Update during break

--- examples/simple-pwa/serviceworker.js
+++ examples/simple-pwa/serviceworker.js
@@ -1,11 +1,13 @@
 const staticCacheName = "PWA";
 
 self.addEventListener("install", e => {
+  console.log("Installerar");
   e.waitUntil(
     caches.open(staticCacheName).then(cache => {
       return cache.addAll([
         "/",
         "/index.html",
+        "https://jsonplaceholder.typicode.com/posts"
       ]);
     })
   );
@@ -24,8 +26,14 @@ self.addEventListener("fetch", e => {
   console.log(e.request.url);
 
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+    caches.match(e.request).then(cacheResponse => {
+      const networkFetch = fetch(e.request)
+        .then(fetchResponse => {
+
+        })
+        .catch()
+
+      return cacheResponse || fetch(e.request);
     })
   );
 })
